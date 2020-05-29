5f00b26695726a30defde9fe3ced94539b643183
ostrosco <ostrosco@fastmail.fm>
2020-05-28 22:58:22 -0600

    Clearing interrupt on timer and first pass at WWVB parsing
    - The interrupt is now cleared when the interrupt fires, so that
      way the interrupt doesn't continuously fire.
    - The timer and GPIOC are now in mutexes so that the interrupt can
      assess them to check the pin and clear the interrupt.
    - Updated to a newer version of the stm32f3xx library.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,30 @@
 #![no_std]
 #![no_main]
 
-extern crate panic_halt; 
+extern crate panic_halt;
+use lazy_static::lazy_static;
 
-use cortex_m::peripheral::NVIC;
+use core::cell::RefCell;
+use cortex_m::{interrupt::Mutex, peripheral::NVIC};
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
-use stm32f3::stm32f303::{self, interrupt, Interrupt};
+use stm32f3::stm32f303::{self, interrupt, Interrupt, GPIOC, TIM2};
 
 static mut TIME_FRAME: [u8; 60] = [0; 60];
+static mut TIME_FRAME_IX: usize = 0;
+lazy_static! {
+    static ref MUTEX_GPIOC: Mutex<RefCell<Option<GPIOC>>> =
+        Mutex::new(RefCell::new(None));
+    static ref MUTEX_TIM2: Mutex<RefCell<Option<TIM2>>> =
+        Mutex::new(RefCell::new(None));
+}
 
 #[entry]
 fn main() -> ! {
     let stm_perip = stm32f303::Peripherals::take().unwrap();
     let mut cortex_perip = cortex_m::Peripherals::take().unwrap();
-    configure_clock(&mut cortex_perip, &stm_perip);
     let rcc = &stm_perip.RCC;
     let gpioc = &stm_perip.GPIOC;
-    let _tim2 = &stm_perip.TIM2;
 
     // Enable the clock on GPIOC.
     rcc.ahbenr.write(|w| w.iopcen().enabled());
@@ -26,6 +33,22 @@ fn main() -> ! {
     gpioc.moder.write(|w| w.moder5().input());
     gpioc.pupdr.write(|w| unsafe { w.pupdr5().bits(0b00) });
 
+    // Configure TIM2 to use as an interrupt for sampling the WWVB signal.
+    configure_clock(&stm_perip);
+
+    cortex_m::interrupt::free(|cs| {
+        MUTEX_GPIOC.borrow(cs).replace(Some(stm_perip.GPIOC));
+        MUTEX_TIM2.borrow(cs).replace(Some(stm_perip.TIM2));
+    });
+
+    // Enable the NVIC interrupt for TIM2.
+    let nvic = &mut cortex_perip.NVIC;
+    unsafe {
+        NVIC::unmask(Interrupt::TIM2);
+        nvic.set_priority(Interrupt::TIM2, 0x01);
+    }
+    NVIC::unpend(Interrupt::TIM2);
+
     unsafe {
         cortex_m::interrupt::enable();
     }
@@ -33,13 +56,9 @@ fn main() -> ! {
     loop {}
 }
 
-fn configure_clock(
-    cortex_perip: &mut cortex_m::Peripherals,
-    stm_perip: &stm32f303::Peripherals,
-) {
+fn configure_clock(stm_perip: &stm32f303::Peripherals) {
     let tim2 = &stm_perip.TIM2;
     let rcc = &stm_perip.RCC;
-    let nvic = &mut cortex_perip.NVIC;
 
     // Disable TIM2 before we start configuring.
     tim2.cr1.write(|w| w.cen().disabled());
@@ -47,21 +66,13 @@ fn configure_clock(
     // Enable APB1_CLOCK going to TIM2.
     rcc.apb1enr.write(|w| w.tim2en().enabled());
 
-    // Enable the NVIC interrupt for TIM2.
-    unsafe {
-        NVIC::unmask(Interrupt::TIM2);
-        nvic.set_priority(Interrupt::TIM2, 0x01);
-    }
-    NVIC::unpend(Interrupt::TIM2);
-
     // Reset TIM2.
     rcc.apb1rstr
         .write(|w| w.tim2rst().set_bit().tim2rst().clear_bit());
 
     // The APB1_CLOCK that drives TIM2 runs at 8MHz by default, so prescale
-    
     // down to 1kHz and then sample every 100ms.
-    tim2.psc.write(|w| unsafe { w.psc().bits(7_999) });
+    tim2.psc.write(|w| w.psc().bits(7_999));
     tim2.arr.write(|w| w.arr().bits(100));
 
     // Apply the settings and reset the timer.
@@ -79,5 +90,33 @@ fn TIM2() {
     static mut BYTES: [u8; 10] = [0; 10];
     static mut INDEX: usize = 0;
 
-    hprintln!("Time fired?").unwrap();
+    let value = cortex_m::interrupt::free(|cs| {
+        let tim2 = MUTEX_TIM2.borrow(cs).borrow();
+        let tim2 = tim2.as_ref()?;
+        tim2.sr.write(|w| w.uif().clear());
+        let refcell = MUTEX_GPIOC.borrow(cs).borrow();
+        let gpioc = refcell.as_ref()?;
+        Some(gpioc.idr.read().idr5().bit())
+    });
+
+    BYTES[*INDEX] = if value.unwrap() { 1 } else { 0 };
+    if *INDEX >= 9 {
+        *INDEX = 0;
+        let byte = match BYTES {
+            [0, 0, 0, 0, 0, 1, 1, 1, 1, 1] => {
+                hprintln!("Got a 1 bit").unwrap();
+                1
+            }
+            [0, 0, 1, 1, 1, 1, 1, 1, 1, 1] => {
+                hprintln!("Got a 0 bit").unwrap();
+                0
+            }
+            _ => {
+                hprintln!("Unknown byte sequence").unwrap();
+                0
+            }
+        };
+    } else {
+        *INDEX += 1;
+    }
 }
